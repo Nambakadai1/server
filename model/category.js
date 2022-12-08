cb2b31a96f3e771d0cee5a67febfb2092db19b09
Pasubathi <73518189+Pasubathi@users.noreply.github.com>
2022-12-08 15:52:53 +0530

    updated

--- model/category.js
+++ model/category.js
@@ -5,20 +5,52 @@ const passwordHash = require("password-hash");
 
 
 const category = new Schema({
-    category:{
+    name:{
         type: String,
         required:true
     },
-    parent_category: {
-      type: String
+    slug: { type: String, index: true },
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      default: null,
     },
+    ancestors: [{
+      _id: {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+        index: true
+    },
+      name: String,
+      slug: String
+    }],
     images: {
       type: Array,
     },
-}   
-,{
+ },   
+ {
   timestamps : true
-  }
+ }
 );
+
+function slugify(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
+category.pre('save', async function (next) {
+  this.slug = slugify(this.name);
+  next();
+});
 
 module.exports = mongoose.model('category', category);
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -20,17 +20,18 @@ const upload = multer({ storage: storage });
 router.post("/", upload.array("images", 5), async (req, res) => {
   const data = req.body;
   const schema = Joi.object({
-    category: Joi.string().required(),
-    parent_category: Joi.string(),
+    name: Joi.string().required(),
+    parent: Joi.string(),
     images: Joi.array()
   });
   try {
     let value = await schema.validateAsync(data);
-    const name = data.category;
+    const name = data.name;
    // console.log("name", name);
     if (!value.error) {
         const existCategory = await Category.findOne({ category:name });
       //  console.log("existCategory", existCategory);
+        const parent = req.body.parent ? req.body.parent : null;
         if(existCategory)
         {
             res.status(200).json({ status: false, message: 'Category Already Exist' });
@@ -48,6 +49,7 @@ router.post("/", upload.array("images", 5), async (req, res) => {
               }
               let category = new Category(data);
               category.save();
+              await buildAncestors(category.id, parent);
               res.status(200).json({ status: true, message: 'Category Added Successfully.', category_id: category.id });
         }
     }
@@ -56,5 +58,72 @@ router.post("/", upload.array("images", 5), async (req, res) => {
     res.status(400).json(error.message);
   }
 });
+
+
+router.get("/", async (req, res) => {
+  try {
+     let category = await Category.find().select({
+      "_id": true, 
+      "name": true,
+      "parent": true,
+      "ancestors.slug": true,
+      "ancestors.name": true })
+      .sort({ _id: -1 });
+      /* let category = await Category.aggregate([
+        {
+            $sort: { order: 1 }
+        },
+        {
+            $graphLookup: {
+                from: 'categories',
+                startWith: '$_id',
+                connectFromField: '_id',
+                connectToField: 'parent',
+                as: 'children'
+            }
+        },
+        {
+            $match: {
+                parent: null
+            }
+        }
+    ]); */
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+});
+
+router.get("/:parent_category", async (req, res) => {
+  const { parent_category } = req.params;
+  console.log("parent_category", parent_category);
+  try {
+     let category = await Category.find().select({
+      "_id": true, 
+      "name": true,
+      "parent": true,
+      "ancestors.slug": true,
+      "ancestors.name": true })
+      .sort({ _id: -1 });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+});
+
+const buildAncestors = async (id, parent_id) => {
+  let ancest = [];
+  try {
+      let parent_category = await Category.findOne({ "_id":    parent_id },{ "name": 1, "slug": 1, "ancestors": 1 }).exec();
+if( parent_category ) {
+         const { _id, name, slug } = parent_category;
+         const ancest = [...parent_category.ancestors];
+         ancest.unshift({ _id, name, slug })
+         const category = await Category.findByIdAndUpdate(id, { $set: { "ancestors": ancest } });
+       }
+    } catch (err) {
+        console.log(err.message)
+     }
+}
 
 module.exports = router;
